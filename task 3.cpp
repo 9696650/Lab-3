927805fd4169f0a34023ac2d159525ba824fac10
9696650 <9696650@stud.kai.edu.ua>
2025-10-17 01:29:18 +0300

    Update task 3.cpp

--- task 3.cpp
+++ task 3.cpp
@@ -1,1 +1,24 @@
+#include <iostream>;
 
+using namespace std;
+
+int main() {
+
+	int var1;		
+	float var2;
+	unsigned short var3;
+
+	var1 = 31765;
+	var2 = -7.293e3;
+	var3 = 26543;
+
+	double var4;
+	int var5;
+	char var6;	
+
+	var4 = var1;		
+	var5 = int(var2);
+	var6 = *(char*)&var3;
+
+	return 0;
+}
