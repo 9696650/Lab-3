fcea01c43c893c7542d47c3fe0d1aacaf291bed2
9696650 <9696650@stud.kai.edu.ua>
2025-10-17 01:13:14 +0300

    Create Task 2.cpp

---
+++ Task 2.cpp
@@ -0,0 +1,26 @@
+#include < iostream>;
+
+using namespace std;
+
+int main() {
+
+	char Var1 = 'T';    
+	char Var2 = 'N';
+	char Var3 = 'C';
+	
+
+	const char const1 = 'B';   
+	const char const2 = 'F';
+	const char const3 = 'M';
+	
+
+	Var1 = 'U';    
+	Var2 = '*';
+	Var3 = '6';
+
+	char char1 = '0x7b';
+	char char2 = '0x4c';
+	char char3 = '0x18';
+
+	return 0;
+}
